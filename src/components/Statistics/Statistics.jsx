dc2ec00293b74100b6c67e9aa0b3c6b3734ed8ec
MozMary <viktor83pochynok@gmail.com>
2023-05-03 22:26:09 +0300

    correction of remark

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -26,7 +26,7 @@ export const Statistics = ({ title, stats }) => {
 };
 
 Statistics.propTypes = {
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.exact({
       id: PropTypes.string.isRequired,
